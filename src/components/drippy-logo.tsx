61528f2532cdfc161573cdb8fcd45c13790dc44f
Tran Duong <dtran2108@gmail.com>
2025-01-02 14:50:33 +0800

    fix drippy logo

--- src/components/drippy-logo.tsx
+++ src/components/drippy-logo.tsx
@@ -4,12 +4,12 @@ export default function DrippyLogo() {
   return (
     <div className="flex items-center justify-center space-x-2">
       <Image
-        src="/logos/Drippy-logo.svg"
+        src="/logos/Drippy-full-logo.svg"
         alt="Drippy logo"
-        width={30}
-        height={30}
+        width={150}
+        height={50}
       />
-      <h1 className="font-bold text-3xl text-black dark:text-white">Drippy.</h1>
+      {/* <h1 className="font-bold text-3xl text-black dark:text-white">Drippy.</h1> */}
     </div>
   );
 }
