e10ec2b2cf99b2fa6202799194181f7278a90dc1
Tran Duong <dtran2108@gmail.com>
2025-01-09 23:53:45 +0800

    add FAQ answers

--- src/components/home-section/faq-section.tsx
+++ src/components/home-section/faq-section.tsx
@@ -9,35 +9,41 @@ export default function FAQSection() {
   const questions = [
     {
       question: 'What is Drippy?',
-      answer: 'Yes. It adheres to the WAI-ARIA design pattern.',
+      answer:
+        'Drippy is a free online 3D design software with real-time collaboration, designed to help entrepreneurs create and produce fashion effortlessly.',
     },
     {
       question: 'How does it work?',
-      answer: 'Yes. It adheres to the WAI-ARIA design pattern.',
+      answer:
+        'Log in and create your first design with our intuitive 3D builder. Then share collaborate with your team and your manufacturers in real-time or export your sewing patterns or 3D files to get started. Finally, connect with a vetted manufacturer to get your sample delivered to your doorstep. Learn more about a process here.',
     },
     {
       question: 'Why 3D design?',
-      answer: 'Yes. It adheres to the WAI-ARIA design pattern.',
+      answer:
+        "It cuts out the need for endless samples, slashes costs, and even helps save the planet by reducing waste. It's faster, smarter, and lets designers play around with ideas without burning a hole in their wallets (or the environment). Pretty cool, right?",
     },
     {
       question: 'Can I use it on the web?',
-      answer: 'Yes. It adheres to the WAI-ARIA design pattern.',
+      answer: 'Yes, definitely! It is 100% browser-based.',
     },
     {
       question: 'How does Drippy help to bring my product to life?',
-      answer: 'Yes. It adheres to the WAI-ARIA design pattern.',
+      answer:
+        'Drippy uses AI to generate precise 2D patterns and even helps you streamline the production process with our network of vetted manufacturers.',
     },
     {
       question: 'Can I just design without making the product?',
-      answer: 'Yes. It adheres to the WAI-ARIA design pattern.',
+      answer: "Totally! You can use Drippy just to design if that's your vibe.",
     },
     {
       question: 'Can I collaborate with other people using Drippy?',
-      answer: 'Yes. It adheres to the WAI-ARIA design pattern.',
+      answer:
+        'Totally, that is why we bring it on the web so everyone can co-create in a same canvas in real-time.',
     },
     {
       question: 'Do you protect my design copyright?',
-      answer: 'Yes. It adheres to the WAI-ARIA design pattern.',
+      answer:
+        'Absolutely. Your designs are yours, and we make sure they stay that way. Drippy uses secure cloud storage and encryption to protect your files, ensuring that only you have access to your creations. We also give you full control over who can view or edit your designs, so you can collaborate without worrying about unauthorized use.',
     },
   ];
 
